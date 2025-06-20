4a4488438366168e0212c5be3347731c4d8395f0
indycrm <Nathaphon.na@chococrm.com>
2025-06-20 10:08:10 +0700

    [fix] add util.

---
+++ Util/JsonUtil.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+
+namespace Disaster_Prediction_And_Alert_System_API.Util
+{
+    public class JsonUtil
+    {
+        public static string ToJsonString(object obj, bool writeIndented = false)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = writeIndented,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
+            };
+
+            return JsonSerializer.Serialize(obj, options);
+        }
+
+        public static T? FromJsonString<T>(string json)
+        {
+            if (string.IsNullOrWhiteSpace(json))
+                return default;
+
+            try
+            {
+                return JsonSerializer.Deserialize<T>(json);
+            }
+            catch (JsonException)
+            {
+                return default;
+            }
+        }
+    }
+}
---
+++ Util/ListUtil.cs
@@ -0,0 +1,10 @@
+﻿namespace Disaster_Prediction_And_Alert_System_API.Util
+{
+    public class ListUtil
+    {
+        public static bool IsNullOrEmpty<T>(IEnumerable<T>? list)
+        {
+            return list == null || !list.Any();
+        }
+    }
+}
